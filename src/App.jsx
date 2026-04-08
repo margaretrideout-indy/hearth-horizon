b7728d7d90ea9c880fbc9f8ac109f74b98c3ef2f
Margaret Rideout <margaretpardy@gmail.com>
2026-04-08 02:27:43 +0000

    Manual code change

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Library from './pages/Library';
 import EmbersChat from './pages/EmbersChat';
 import Canopy from './pages/Canopy';
 
+// This layout only applies to internal pages (Library, Chat, etc.)
 const AppLayout = () => (
   <div className="flex min-h-screen bg-[#1A1423]">
+    {/* Sidebar is fixed/present ONLY for these routes */}
     <Sidebar />
-    <main className="flex-1 overflow-y-auto">
+    <main className="flex-1 md:ml-64 overflow-y-auto">
       <Outlet /> 
     </main>
   </div>
@@ -19,14 +21,17 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* GroveTiers is RENDERED ALONE - No Sidebar, Full Screen, App/Web Ready */}
         <Route path="/" element={<GroveTiers />} />
 
+        {/* These routes will have the Sidebar */}
         <Route element={<AppLayout />}>
           <Route path="/library" element={<Library />} />
           <Route path="/embers" element={<EmbersChat />} />
           <Route path="/canopy" element={<Canopy />} />
         </Route>
 
+        {/* Redirects */}
         <Route path="/grove" element={<Navigate to="/" replace />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
