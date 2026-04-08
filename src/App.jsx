77b874fe67f51558a1036c5456dd073bc37b4c59
Margaret Rideout <margaretpardy@gmail.com>
2026-04-08 01:32:44 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import Sidebar from './components/Sidebar'; 
+import Sidebar from './components/layout/Sidebar'; 
 import GroveTiers from './pages/GroveTiers';
 import Library from './pages/Library';
 import EmbersChat from './pages/EmbersChat';
